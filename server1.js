160ea26772bbc432f0718ee0ca8d2ced9ec615e7
GulraizKhan-GIS <52384630+GulraizKhan-GIS@users.noreply.github.com>
2024-12-10 22:01:32 +0500

    server1.js

--- server1.js
+++ server1.js
@@ -8,11 +8,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Route to serve the index.html file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.get('/t', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/test', '1.html'));   
-  });
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
